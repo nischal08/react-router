472c7c9dcc1fa13e73a603125503c2dc4a4ecbf2
nischal08 <nischal.karanjit@gmail.com>
2020-05-06 14:33:55 +0545

    navigating programatically

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import axios from './../../../axios';
 import Post from './../../../components/Post/Post';
 import './Posts.css';
-import { Link } from 'react-router-dom';
 
 class Posts extends Component {
   state = {
@@ -13,7 +12,7 @@ class Posts extends Component {
     console.log(this.props);
 
     axios
-      .get('/posts')
+      .get('/posts/')
       .then((response) => {
         const posts = response.data.slice(0, 4);
         const updatedPosts = posts.map((post) => {
@@ -28,22 +27,24 @@ class Posts extends Component {
   }
 
   postSelectedHandler = (id) => {
-    this.setState({ selectedPostId: id });
+    //this.props.history.push({ pathname: '/' + id });
+    this.props.history.push('/' + id);
   };
   render() {
     let posts = <p style={{ textAlign: 'center' }}>Something went wrong...</p>;
     if (!this.state.error) {
       posts = this.state.posts.map((post) => {
         return (
-          <Link key={post.id} to={'/' + post.id}>
-            <Post
-              clicked={() => {
-                this.postSelectedHandler(post.id);
-              }}
-              title={post.title}
-              author={post.author}
-            />
-          </Link>
+          //  <Link key={post.id} to={'/' + post.id}>
+          <Post
+            key={post.id}
+            clicked={() => {
+              this.postSelectedHandler(post.id);
+            }}
+            title={post.title}
+            author={post.author}
+          />
+          //  </Link>
         );
       });
     }
