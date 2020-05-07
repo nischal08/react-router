834acad27544207a8c82110e1a03673221b68664
nischal08 <nischal.karanjit@gmail.com>
2020-05-07 16:40:43 +0545

    Deploying the sub directory react app in server

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import Blog from './containers/Blog/Blog';
 
 class App extends Component {
-  render () {
+  render() {
     return (
+      // <BrowserRouter basename="/my-app" > for using the react in sub directory such as my-app
       <BrowserRouter>
         <div className="App">
           <Blog />
