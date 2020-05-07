9878dd11145011f1d6466eb0d85f1a7469f769c5
nischal08 <nischal.karanjit@gmail.com>
2020-05-07 15:47:19 +0545

    lazy routing with hoc

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
-
+import { Route, NavLink, Switch /* Redirect */ } from 'react-router-dom';
+import asyncComponent from './../../hoc/asyncComponent';
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+//import NewPost from './NewPost/NewPost';
+const AsyncNewPost = asyncComponent(()=>{
+  return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
   state = {
-    auth: false,
+    auth: true,
   };
   render() {
     return (
@@ -47,7 +50,7 @@ class Blog extends Component {
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
         <Switch>
           {this.state.auth ? (
-            <Route path="/new-post" component={NewPost} />
+            <Route path="/new-post" component={AsyncNewPost} />
           ) : null}
           <Route path="/posts" component={Posts} />
           <Route render={() => <h1>Not Found</h1>} />
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -13,7 +13,7 @@ class Posts extends Component {
 
     componentDidMount () {
         console.log( this.props );
-        axios.get( '/posts' )
+        axios.get( '/posts/' )
             .then( response => {
                 const posts = response.data.slice( 0, 4 );
                 const updatedPosts = posts.map( post => {
